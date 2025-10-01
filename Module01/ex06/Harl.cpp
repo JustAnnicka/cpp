ba9b873a0dc6b9c7cd966c18acdb427cf0b977b4
aehrl <aehrl@student.42malaga.com>
2025-10-01 14:12:56 +0200

    ex06 HarlFilter, using switches

---
+++ Module01/ex06/Harl.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aehrl <aehrl@student.42malaga.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/26 12:17:51 by aehrl             #+#    #+#             */
+/*   Updated: 2025/10/01 14:12:20 by aehrl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(){}
+
+Harl::~Harl(){}
+
+void Harl::debug()
+{
+	std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void Harl::info()
+{
+	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning()
+{
+	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error()
+{
+	std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
+}
+void Harl::complain(std::string level)
+{
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	void (Harl::*func[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	
+	for (int i = 0; i < 4; i++)
+	{
+		if(!level.compare(levels[i]))
+		{
+			switch (i)
+			{
+				case 0:
+					(this->*func[0])();
+				case 1:
+					(this->*func[1])();
+				case 2:
+					(this->*func[2])();
+				case 3:
+					(this->*func[3])();
+			}
+			return ;
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]";
+	
+}
