48d656db3abeb625ffb2f5c90196b8646db00f9c
JustAnnicka <annicka.ehrl@gmail.com>
2026-02-04 16:24:06 +0100

    Working templates for swap, min and max. Adjusted main to showcase other parameter types

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -1,31 +1,29 @@
 #include "whatever.hpp"
-
-//SUBJECT EXAMPLE:
+#include <iostream>
 
 int main( void ) {
 int a = 2;
 int b = 3;
-::swap( a, b );
+std::cout << "=== Before int swap: ===" << std::endl;
 std::cout << "a = " << a << ", b = " << b << std::endl;
-std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+::swap( a, b );
+std::cout << "=== After int swap: ===" << std::endl;
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 std::string c = "chaine1";
 std::string d = "chaine2";
-::swap(c, d);
+std::cout << "=== Before string swap: ===" << std::endl;
 std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+::swap(c, d);
+std::cout << "=== After string swap: ===" << std::endl;
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+std::cout << "=== Other parameter types: ===" << std::endl;
+std::cout << "min(3.5, 4.5) = " << ::min<double>(3.5, 4.5) << std::endl;
+std::cout << "min(2.0f, 42.0f) = " << ::min<float>(2.0f, 42.0f) << std::endl;
+std::cout << "min('c', 'f') = " << ::min<char>('c', 'f') << std::endl;
+
 return 0;
 } 
-
-
-/*
-expected output:
-a = 3, b = 2
-min(a, b) = 2
-max(a, b) = 3
-c = chaine2, d = chaine1
-min(c, d) = chaine1
-max(c, d) = chaine2
-
-*/
--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -1,15 +1,22 @@
-/* Implement the following function templates:
-• swap: Swaps the values of two given parameters. Does not return anything.
-• min: Compares the two values passed as parameters and returns the smallest one.
-If they are equal, it returns the second one.
-• max: Compares the two values passed as parameters and returns the greatest one.
-If they are equal, it returns the second one.
-These functions can be called with any type of argument. The only requirement is
-that the two arguments must have the same type and must support all the comparison
-operators.
-Templates must be defined in the header files */
-
 #ifndef WHATEVER_HPP
  #define WHATEVER_HPP
+ 
+template <typename T> void swap(T &x, T &y){
+    T swap = x;
+    x = y;
+    y = swap;
+}
+
+template <typename T> T min(T x, T y){
+    if (x == y)
+        return (y);
+    return (x < y) ? x : y;
+}
+
+template <typename T> T max(T x, T y){
+    if (x == y)
+        return (y);
+    return (x > y) ? x : y;
+}
 
 #endif
