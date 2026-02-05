712deacfcf83b2c6d4b401cdf706d36868b9f078
aehrl <aehrl@student.42malaga.com>
2026-02-05 16:42:49 +0100

    working iter template that now also checks for if len is bigger than array size. Also added a template function into the main.cpp

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -1,18 +1,21 @@
 
+#ifndef ITER_HPP
+ #define ITER_HPP
+
 #include <iostream>
-//template <typename A, typename L, typename F> void iter(A ptr[], L const len, void *f(F)){
-template <typename T, typename F> void iter(T ptr[],const int len, F function){
-    //A is address to an array -> A &ptr
-    //L is len of array (can only be int or size_t ??)
-    //F is function passed to each element of array
-    // -> function passed can take the reference as const and non-const
-    
-    //can I use the iterator header?
+
+template <typename T, typename F> void iter(T &ptr, const size_t len, F function){
     if (!ptr)
         return ;
-    for (int i = 0; i < len; i++)
+    if (len > (int)sizeof(ptr) / sizeof(ptr[0]))
+    {
+        std::cout << "Error\nLen does not match array size" << std::endl;
+        return ;
+    }
+    for (size_t i = 0; i < len; i++)
+    {
         function(ptr[i]);
- 
-} 
+    }
+}
 
-//I think i might need to add type specific functions -> needed for string!!!
+#endif
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -3,70 +3,68 @@
 #include <iostream>
 
 void test_intconst_function(const int &ref){
-    std::cout << ref;
+    std::cout << ref<< " ";
 }
 void test_int_function(int &ref){
-    std::cout << ref;
+    std::cout << ref<< " ";
 }
 
 void test_charconst_function(const char &ref){
-    std::cout << ref;
+    std::cout << ref<< " ";
 }
 
-void test_char_function(char &ref){
-    std::cout << ref;
+void test_char_function(char & ref){
+    std::cout << ref<< " ";
 }
 
-void test_stringconst_function(const std::string &ref){
-    std::cout << ref;
+void test_stringconst_function(const std::string & ref){
+    std::cout << ref<< " ";
+}
+
+template < typename T >
+void t_print(T & ref){
+    std::cout << ref << " ";
 }
 
 int main( void ) {
 int a[] = {1, 2, 3, 4};
 char b[] = {'c', 'b', 'd', 'e'};
-char c[4] ="BYE";
-//char d[3];
-int e;
-char f;
-const int x = -20;
-std::string str[2] = {"Hello", "Bye"};
-const std::string str2[3] = {"Const: ", "Hello", "Bye"};
+char const  c[4] ="BYE";
+std::string str[2] = {"Hello ", "Bye"};
+const std::string str2[3] = {"Const: ", "Hello ", "Bye"};
 
-::iter(a, sizeof(a) / sizeof(a[0]), test_int_function);
+std::cout << "==== Normal function test ===" << std::endl;
+iter(a, sizeof(a) / sizeof(a[0]), test_int_function);
 std::cout << std::endl;
 ::iter(a, sizeof(a) / sizeof(a[0]), test_intconst_function);
 std::cout << std::endl;
-::iter(b, sizeof(b) / sizeof(b[0]), test_charconst_function);
+::iter(b, sizeof(b) / sizeof(b[0]), test_char_function);
 std::cout << std::endl;
 ::iter(c, sizeof(c) / sizeof(c[0]), test_charconst_function);
-std::cout << std::endl;
 ::iter(str, 2, test_stringconst_function);
 std::cout << std::endl;
 ::iter(str2, 3, test_stringconst_function);
-
 std::cout << std::endl;
-//FOR SOME REASON IF I USE THE ADDRESS IT COUNTS AS INITIALSED 
-std::cout << &e << std::endl;
-std::cout << sizeof(e)/sizeof(e) << std::endl;
-//the type for the function needs and that is passed into the iter function dont match need to check for this?.
-::iter(&e, sizeof(e)/sizeof(e), test_charconst_function);
+std::cout << "\n==== Template function test ===" << std::endl;
+::iter(a, sizeof(a) / sizeof(a[0]), t_print<int>);
 std::cout << std::endl;
-//Here again passing the address will cause the function to work
-::iter(&e, sizeof(f)/sizeof(f), test_intconst_function);
+::iter(b, sizeof(b) / sizeof(b[0]), t_print<char>);
 std::cout << std::endl;
-//gives giberish as well
-::iter(b, sizeof(b) / sizeof(b[0]), test_intconst_function);
+::iter(str, 2, t_print< std::string>);
 std::cout << std::endl;
-//giving bigger number than array size seems fine for char array 
-::iter(b, 10, test_charconst_function);
+::iter(str2, 3, t_print<const std::string>);
 std::cout << std::endl;
-//giving bigger number than array size seems fine for char array 
-::iter(c, 10, test_charconst_function);
+std::cout << "\n==== Len Error ===" << std::endl;
+::iter(str, -2, test_stringconst_function);
 std::cout << std::endl;
-//this should not be allowed (why does the function accept it on compile??)
-::iter(c, x, test_charconst_function);
+::iter(str2, 5, test_stringconst_function);
+std::cout << std::endl;
+::iter(c, 20, test_charconst_function);
+std::cout << std::endl;
+::iter(a, 20, test_intconst_function);
 std::cout << std::endl;
 
-//::iter(str, str.length(), test_charconst_function);
+return (0);
 
 }
+
