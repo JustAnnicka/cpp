bea8b2ba0893e8c2cf3728043b84c394a815284d
JustAnnicka <aehrl@student.42malaga.com>
2026-02-10 15:36:38 +0100

    simplified version of easyfind using std::find and also added an excpetion class

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -3,44 +3,39 @@
  #define EASYFIND_HPP
 
 #include <iostream>
-//Following Headers are STL headers
-//#include <array> is c++11
-//#include <forward_list> is c++11
-
-// Sequence Containers 
 #include <vector>
 #include <deque>
 #include <list>
-
-/* // Container Adaptors
-#include <stack>
-#include <queue> */
-
-
-/* //Associatice Containers
-#include <set>
-#include <multiset>
-#include <map>
-#include <multimap>
- */
-
-
+//#include <string>
 #include <algorithm>
-//#include <iterator> // these are the pointer like objects (needed to connect stl algoriths with the containers)
-// check if i am allowed to use iterators. (they should be included in the other headers actually)
-//template <typename T> T::iterator easyfind(T container, int i){
-template <typename T> void easyfind(T container, int i){
+#include <stdexcept>
+
+class ValNotFound : public std::exception{
+	public:
+		virtual const char* what() const throw(){
+			return ("Value not found.");
+		}
+};
+
+template <typename T> typename T::iterator easyfind(T container, int i){
+
+	typename T::iterator it = std::find(container.begin(), container.end(), i);
+	
+	if (it == container.end())
+		throw(ValNotFound());
+	//std::cout << "The first occurace is: " << *it << std::endl;
+	//std::cout << "The first occurace is: " << *it << std::endl;
+	return (it);
+} 
+/* 
+template <> void easyfind <std::string>(std::string container, int i){
 
 	typename T::iterator it;
-	//typename T::iterator needle;
 
 	int array[1];
 	array[0] = i;
-
 	
-	//.begin() & .end(),  size(), resize() works for:
-	// ->vector, deque, list
-	
+	//find first of works for chars and ints (not for Strings)
 	it = find_first_of(container.begin(), container.end(), array, array+1);
 	
 	if (it != container.end())
@@ -52,10 +47,7 @@ template <typename T> void easyfind(T container, int i){
 		std::cout << i << " not found in container" << std::endl;
 
 	//maybe add checks for other types of container (ie if its a char or string it should give other output)
-
-
-	// Dont think this is correct as it needs an iterator for the last two values (ie in an array)
-} 
+}  */
 
 #endif
 //Assuming T is a container of integers
@@ -64,8 +56,3 @@ template <typename T> void easyfind(T container, int i){
 // ---> if no occurance is found either:
 // -----> throw and expetion
 // -----> OR return an error value of your choice
-
-// For inspo check how standard containers behave
-
-
-// You dont have to handle associative containers!!
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -1,14 +1,29 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <deque>
+#include <list>
+#include <string>
 
 int	main()
 {
-	int mychars[] = {'a','b','c','A','B','C'};
-  	std::vector<char> haystack (mychars,mychars+6);
-  	
-	
-	std::vector<char>::iterator it;
-	::easyfind(haystack, 'A');
-	::easyfind(haystack, 'z');
+	int ints[] = {'a','b','c','A','B','C'};
+  	std::vector<int> haystack (ints,ints+6);
+
+	char chars[] = {'a','b','c','A','B','C'};
+  	std::deque<char> haystack2 (chars,chars+6);
+	/* 
+	std::string str[] = {"Hallo","this","is","a","string","!!!"};
+  	std::list<std::string> haystack3 (chars,chars+6); */
+	try{
+		::easyfind(haystack, 'A');
+		::easyfind(haystack2, 'a');
+		::easyfind(haystack, 'z');
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	//::easyfind(haystack3, 'a');
 	//std::cout << "It is " << *it << std::endl;
 
 	return (0);
