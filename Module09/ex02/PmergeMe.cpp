d832aaf2fff7c972001499b8b895c45be5569a70
JustAnnicka <aehrl@student.42malaga.com>
2026-02-24 19:46:03 +0100

    For 11 elements i get the max chain atm

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -18,12 +18,14 @@ sequence
 */
 
 PmergeMe::PmergeMe(){
+	_tracker = 0;
 	//std::cout << "PmergeMe constructor called" << std::endl;
 	//do i have to init time to NULL?
 
 }
 
 PmergeMe::PmergeMe(int argc, char *argv[]){
+	_tracker = 0;
 	//std::cout << "PmergeMe string array constructor called" << std::endl;
 	this->johnsonFord(argc, argv);
 }
@@ -31,6 +33,7 @@ PmergeMe::PmergeMe(int argc, char *argv[]){
 PmergeMe::~PmergeMe(){}
 
 void PmergeMe::johnsonFord(int argc, char *argv[]){
+	_tracker = 0;
 	this->fill_containers(argc, argv);
 	//start deque timer
 	dequeSort(_deque);
@@ -43,75 +46,260 @@ void PmergeMe::johnsonFord(int argc, char *argv[]){
 
 }
 
-//Use a template for the pairs
-
-//Make pairs functions -> make a pair from a deque and then push to holder
-//template <typename T>  std::list<T> make_pairs_deque(T container){
-/* template <typename T>  void creating_pairs(T container){
-	T<T> holder;
-	std::pair
-	bool odd = false;
-	size_t size = container.size() / 2;
-	static int count; //just for me to visualise the the count (delete this later)
-	if (container.size() % 2 != 0)
-	{
-		odd = true;
-		size++;
-	}
-	//std::pair<T> pair;
-	for (size_t i = 0; i < size; i++){
-
-		holder.push(container.back());
-		container.pop_back();
-		holder.push(container.back());
-		container.pop_back();
-	} 
-} */
-//sort pair functions
-
-template <typename T> bool first_is_bigger(T a, T b){
+/* template <typename T> bool first_is_bigger(T *a, T *b){
 
 	//if (typeid(a->second) != typeid(std::pair<std::deque<unsigned int>, std::deque<unsigned int>))
 	//if (typeid(a->second) != typeid(std::pair<unsigned int, unsigned int>))
-	if (typeid(a) == typeid(unsigned int))
-		return(a > b);
-	return (a>b);
-	/* else if (typeid(a->second) != typeid(std::pair<unsigned int, unsigned int>))
+
+	if (typeid(a->second) != typeid(std::pair<unsigned int, unsigned int>))
 		return (sort_pairs(a->second, b->second));
 	else
-		return (a->second > b->second); */
+		return (a->second > b->second);
+} */
 
+//
+//template <typename T> void deque_iterate_pairing(T container){
+	//std::deque<unsigned int>;
 	
-		// by comparing the second I should auto know if I am one away from pair
-	// Maybe what makes more sense is to 
-	/* if (typeid(*T) == type(std::deque)){
-		for (T::iterator it = container.begin(); it != s.end(); ++it)
-		{
-			if (typeid(container->first) != typeid(std::pair<std::deque<unsigned int>, std::deque<unsigned int>))
+/* 	if (container.size() > 1){	
+		std::cout << "Pairing #2" << std::endl;
+		std::pair<T, T> pairing;
+		std::deque<std::pair<T, T> > holder;
+		size_t size = container.size() / 2;
+		for (size_t i = 0; i < size; i++){
+			typename T::iterator it = container.begin();
+			//std::deque<std::pair<T, T> >  x = container.front();
+			pairing = std::make_pair(it, it+1);
+			container.pop_front();
+			container.pop_front();
+			holder.push_back(pairing);
 
+			//PRINT
+		//	std::cout << holder[i].first << ", " << holder[i].second.first << " | ";
+			//std::cout << holder[i].second.first << ", " << holder[i].second.second << " | ";
+
+		} */
+	/* 	for (size_t j = 0; j < holder.size(); j++){
+
+			if (first_is_bigger(holder[j].first, holder[j].second))
+			{
+				T ptr = holder[j].first;
+				holder[j].first = holder[j].second;
+				holder[j].second = ptr;
+			}
+		} */
+//	}
+//}
+
+void PmergeMe::recursive_pairing(std::deque<t_pairs> aux){
+	std::deque<t_pairs> holder; //might not need holder
+	size_t size = aux.size() / 2;
+	static int id;
+	id++;
+	for (size_t i = 0; i < size; i++){
+		unsigned int x = aux[i].max;
+		//aux.pop_front();
+		t_pairs p(x, aux[i + 1].max);
+		p.id = id;
+		holder.push_back(p);
+		_dsort.push_back(p);
+		//i = i + 1;
+		aux.pop_front();
+	}
+	for(size_t z = 0; z < size; z++)
+		aux.pop_front();
+	//JUST FOR PRINTING PURPOSES
+	std::cout << "Pairing #" << id << std::endl;
+	for (size_t j = 0; j < holder.size(); j++){
+		std::cout << holder[j].min << " " << holder[j].max << ", ";
+	}
+	if (!aux.empty())
+	{
+		std::cout << "r => ";
+		for (size_t z = 0; z < aux.size(); z++){
+			std::cout << aux[0].max << ", ";
 		}
+	}
+	std::cout << std::endl;
+	//END OF PRINT
 
+	if (holder.size() != 1){
+		recursive_pairing(holder);
+	}
 
+/* 	if (holder.size() != 1)
+	{
+		recursive_pairing(holder);
+		_dsort.push_back(aux[0].max);
+		_dsort.push_back(aux[0].min);
+	}
+	else
+	{
+		for (size_t j = 0; j < holder.size(); j++){
+			_dsort.push_back(holder[j].min);
 	} */
+
+	//could do insertion here with the deque
+}
+
+void PmergeMe::pairing(std::deque<unsigned int> aux){
+	//std::deque<t_pairs> holder;
+	size_t size = aux.size() / 2;
+	for (size_t i = 0; i < size; i++){
+		unsigned int x = aux.front();
+		aux.pop_front();
+		t_pairs p(x, aux.front());
+		p.id = 0;
+		_dsort.push_back(p);
+		aux.pop_front();
+	}
+	//JUST FOR PRINTING PURPOSES
+	std::cout << "Pairing #0" << std::endl;
+	for (size_t j = 0; j < _dsort.size(); j++){
+		std::cout << _dsort[j].min << " " << _dsort[j].max << ", ";
+	}
+	if (!aux.empty())
+	{
+		std::cout << "r => ";
+		for (size_t z = 0; z < aux.size(); z++){
+			std::cout << aux[0] << ", ";
+		}
+	}
+	std::cout << std::endl;
+	//END OF PRINT
 
 }
 
-/* for (size_t i = 0; i < size; i++){
-		one.push_back(aux.front());
-		aux.pop_front();
-		two.push_back(aux.front());
-		aux.pop_front();
-		pairing = make_pair(one, two);
-		holder.push_back(pairing);
-	} */
+/* void PmergeMe::insertion(){
+	//INSERTION A (BIGGER)
+	std::deque<unsigned int> main;
+	int count = 0;
+	for (int i = (int)_dsort.size() - 1; i >= 0; i--)
+	{	
+		std::cout << "ENTER size: " << _dsort.size() << " i : " << i << std::endl;
+		int x = i;
+		if (!main.empty())
+		{
+			while(i > 1 && _dsort[i].max != main[0])
+				i--;
+		}
+		//if (count == 0)
+			main.push_front(_dsort[i].max);
+		//else
+	//		main.push_front(_dsort[i].min); 
+		while(i > 1 && (int)_dsort[i].id == _dsort[x].id)
+			i--;
+		count++;
+	}
+	for (size_t j = 0; j < main.size(); j++){
+		std::cout << main[j] << " ";
+	}
+	std::cout << std::endl;
+} */
+/* void PmergeMe::insertion(){
 
+	std::deque<unsigned int> main;
+	int count = 0;
+	for (int i = (int)_dsort.size() - 1; i >= 0; i--)
+	{	
+		int x = i;
+		if (count == 0)
+			main.push_front(_dsort[i].max);
+		else{
+			int z = 0;
+			while (_dsort[z].id == 0)
+			{
+				if (_dsort[z].max == _dsort[i].max)
+					{
+						main.push_front(_dsort[z].min);
+						break ;
+					}
+				z++;
+			}
+		}
+		
+		//else
+	//		main.push_front(_dsort[i].min); 
+		while(i > 1 && (int)_dsort[i].id == _dsort[x].id)
+			i--;
+		count++;
+	}
+	for (size_t j = 0; j < main.size(); j++){
+		std::cout << main[j] << " ";
+	}
+	std::cout << std::endl;
+
+} */
+void PmergeMe::insertion(){
+
+//	std::deque<unsigned int> main;
+	std::deque<unsigned int> chain;
+	t_pairs top = _dsort.back();
+	chain.push_front(top.max);
+	chain.push_front(top.min);
+	// I dont think i need to add the other id to the _dsort just the initial pairing
+	//FIND MATCH OF MAX
+	int i = (int)_dsort.size() - 2; // this is the previous from top layer
+	while (i){
+		if (top.max == _dsort[i].max)
+		{
+			chain.push_front(_dsort[i].min);
+			break ;
+		}
+		i--;
+	}
+	i = (int)_dsort.size() - 2;
+	while (i){
+		if (top.min == _dsort[i].max)
+		{
+			chain.push_front(_dsort[i].min);
+			break ;
+		}
+		i--;
+	}
+	for (size_t j = 0; j < chain.size(); j++){
+		std::cout << chain[j] << ", ";
+	}
+	//could do for lower function?
+	/* int count = 0;
+	for (int i = (int)_dsort.size() - 1; i >= 0; i--)
+	{	
+		int x = i;
+		if (count == 0)
+			main.push_front(_dsort[i].max);
+		else{
+			int z = 0;
+			while (_dsort[z].id == 0)
+			{
+				if (_dsort[z].max == _dsort[i].max)
+					{
+						main.push_front(_dsort[z].min);
+						break ;
+					}
+				z++;
+			}
+		}
+		
+		//else
+	//		main.push_front(_dsort[i].min); 
+		while(i > 1 && (int)_dsort[i].id == _dsort[x].id)
+			i--;
+		count++;
+	}
+	for (size_t j = 0; j < main.size(); j++){
+		std::cout << main[j] << " ";
+	}
+	std::cout << std::endl; */
+
+}
 
 void PmergeMe::dequeSort(std::deque<unsigned int> aux){
-	std::pair<unsigned int, unsigned int> pairing;
+	pairing(aux);
+	recursive_pairing(_dsort);
+	insertion();
+	//recursive_pairing(aux);
+/* 	std::pair<unsigned int, unsigned int> pairing;
 	std::deque<std::pair<unsigned int, unsigned int> > holder;
-	//std::deque<std::pair<std::deque<unsigned int>, std::deque<unsigned int> > > holder;
-	//std::deque<unsigned int> one;
-	//std::deque<unsigned int> two;
 	size_t size = aux.size() / 2;
 	for (size_t i = 0; i < size; i++){
 		unsigned int x = aux.front();
@@ -120,6 +308,7 @@ void PmergeMe::dequeSort(std::deque<unsigned int> aux){
 		aux.pop_front();
 		holder.push_back(pairing);
 	}
+	//START OF PRINT
 	std::cout << "Pairing #1" << std::endl;
 	for (size_t j = 0; j < holder.size(); j++){
 		std::cout << holder[j].first << " " << holder[j].second << ", ";
@@ -132,75 +321,19 @@ void PmergeMe::dequeSort(std::deque<unsigned int> aux){
 		}
 	}
 	std::cout << std::endl;
+	//END OF PRINT
 	for (size_t z = 0; z < holder.size(); z++){
 		if (first_is_bigger(holder[z].first, holder[z].second))
 		{
 			unsigned int n = holder[z].first;
 			holder[z].first = holder[z].second;
 			holder[z].second = n;
-			//std::deque<std::pair<unsigned int, unsigned int> temp = holder[z]
 		}
 		std::cout << holder[z].first << " " << holder[z].second << ", ";
 	}
 	std::cout << std::endl;
-	//sort_pairs(&holder);
+	deque_iterate_pairing(holder); */
 }
-
-/* void PmergeMe::MakePairs(std::deque<std::set<unsigned int> > container){
-	std::deque<std::pair<std::set<unsigned int>, std::set<unsigned int> > >holder;
-	//std::deque<std::set<std::pair<std::set<unsigned int> >> holder;
-	//std::set<std::pair<std::set<unsigned int> >> set;
-	std::pair<std::set<unsigned int>, std::set<unsigned int> > pairs;
-	size_t size = container.size();
-	bool 		odd = false;
-	if (size % 2 != 0)
-		odd = true;
-
-	for (size_t i = 1; i < size; i++){
-		if(*container[i-1].rbegin() < *container[i].rbegin())
-			pairs = std::make_pair(container[i - 1], container[i]);
-		else
-			pairs = std::make_pair(container[i], container[i - 1]);
-		holder.push_back(pairs);
-		size_t x = i + 1;
-		i = x;
-	}
-	for (size_t j = 0; j < holder.size(); j++){
-		std::cout << holder[j].first << " " << holder[j].second << ", ";
-	}
-	std::cout << std::endl; */
-/* 	if (odd == true){
-		pairing.insert(container.back());
-		holder.push_back(pairing);
-	} */
-//}
-
-//create pairs -> do a type identify for the sorting. If the first / second is notn an unsigned int but a pair call function again. then return the result
-
-//CREATE PAIR TEMPLATE FUNCTION THAT RETURNS DEQUE ENTRY
-
-
-
-/* void PmergeMe::dequeSort(){
-	std::deque<unsigned int>	holder;
-	bool 		odd = false;
-	//create a set of half the size of the (if it is not even then one extra element)
-	//size_t set_size = _deque.size() / 2;
-	if (_deque.size() % 2 != 0){
-		//set_size++;
-		odd = true;
-	}
-	std::set<std::pair<int, int> > s;
-	for (size_t i = 1; i < _deque.size(); i+= 2){
-		std::pair<int, int> p = std::make_pair(_deque[i - 1], _deque[i]);
-		s.insert(p);
-	}
-	if (odd == true)
-		holder.push_back(_deque.back());
-	for (std::set<std::pair<int, int> >::iterator it = s.begin(); it != s.end(); ++it)
-    	std::cout << it->first << " " << it->second << ", ";
-	std::cout << std::endl;
-} */
 
 void PmergeMe::fill_containers(int argc, char *argv[])
 {
@@ -233,7 +366,6 @@ void PmergeMe::fill_containers(int argc, char *argv[])
 		_deque.push_back(static_cast<unsigned int>(n));
 		_vector.push_back(static_cast<unsigned int>(n));
 	}
-	//Start timer
 }
 
 // === EXCEPTIONS ===
